6f695252bfa938fe22703f078b5a4356bcff1425
S. Everett Abbott <eabbott@collineargroup.com>
2019-06-07 17:06:34 -0700

    Adds basic setup for visibility on tooltip.

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from '@collinear/react-tooltip'
+import { CollinearTooltip } from '@collinear/react-tooltip'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <CollinearTooltip>
+        <div>
+          My Component
+        </div>
+      </CollinearTooltip>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+// import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export class CollinearTooltip extends PureComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isVisible: false,
+      mouseX: 0,
+      mouseY: 0,
+      isBottom: false
+    }
+  }
+
+  onMouseEnter = (e) => this.setState({ isVisible: true })
+
+  onMouseLeave = (e) => this.setState({ isVisible: false })
+  
+  onMouseMove = (e) => {
+
+  }
+
+  renderChildren = () => {
+    const newProps = {
+      onMouseEnter: this.onMouseEnter,
+      onMouseLeave: this.onMouseLeave,
+      onMouseMove: this.onMouseMove
+    }
+
+    if (typeof this.props.children === 'function') {
+      return this.props.children(newProps) 
+    } else if (this.props.children) {
+      return React.cloneElement(this.props.children, newProps)
+    }
+  }
+
   static propTypes = {
-    text: PropTypes.string
+    children: PropTypes.any
   }
 
   render() {
     const {
-      text
-    } = this.props
+      isVisible
+    } = this.state
+
+    const styles = {
+      visibility: isVisible ? 'visible' : 'hidden'
+    }
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>
+        <div style={styles}>
+          Example Component:
+        </div>
+        {this.renderChildren()}
       </div>
+
     )
   }
 }
